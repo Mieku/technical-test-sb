5bbd97027ca8a66ba212102c956958523a8f9117
Michael Altair <m.altair@live.com>
2019-11-15 13:23:30 -0400

    Tweaked the UI to better handle multiple outputs

---
+++ Technical Test SB/Assets/Scripts/OutputDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class OutputDisplay : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _outputRenderer, _positionRenderer;
+
+    #region Button Inputs
+    public void NextBtnPressed()
+    {
+        // TODO: Build Next Button
+        throw new NotImplementedException();
+    }
+
+    public void PreviousBtnPressed()
+    {
+        // TODO: Build Prev Button
+        throw new NotImplementedException();
+    }
+    #endregion
+}
--- Technical Test SB/Assets/Scripts/SystemUI.cs
+++ Technical Test SB/Assets/Scripts/SystemUI.cs
@@ -1,8 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
 
 public class SystemUI : MonoBehaviour
 {
-    
+    [Header("Input Fields")]
+    [SerializeField] TMP_InputField _horizontalCellInput;
+    [SerializeField] TMP_InputField _verticalCellInput;
+    [SerializeField] TMP_InputField _kingInput;
+    [SerializeField] TMP_InputField _queenInput;
+    [SerializeField] TMP_InputField _bishopInput;
+    [SerializeField] TMP_InputField _rookInput;
+    [SerializeField] TMP_InputField _knightInput;
+
+    [Header("Output Fields")]
+    [SerializeField] OutputDisplay _outputDisplay;
+    [SerializeField] TextMeshProUGUI _errorRenderer;
+
+    #region Button Inputs
+    public void CalculateBtnPressed()
+    {
+        // TODO: Build Calculate Button
+        throw new NotImplementedException();
+    }
+    #endregion
+
 }
