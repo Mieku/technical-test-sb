a54733e7d36dac6753fc8cf117d13ba38b24e8cb
Michael Altair <m.altair@live.com>
2019-11-17 13:36:26 -0400

    Modified the output display to actually display a board and pieces

---
+++ Technical Test SB/Assets/Scripts/ChessPieceRenderer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChessPieceRenderer : MonoBehaviour
+{
+    [SerializeField] Image _pieceRenderer, _tileRenderer;
+
+    [SerializeField] Sprite _kingSpr, _queenSpr, _bishopSpr, _rookSpr, _knightSpr;
+    [SerializeField] Color _lightTileColour, _darkTileColour;
+
+    public void SetPieceDisplay(string pieceCode)
+    {
+        Sprite pieceSprite = DetermineSprite(pieceCode);
+        if(pieceSprite == null)
+        {
+            _pieceRenderer.enabled = false;
+        } else
+        {
+            _pieceRenderer.enabled = true;
+            _pieceRenderer.sprite = pieceSprite;
+        }
+    }
+
+    public void SetTileColour(bool isEven)
+    {
+        if(isEven)
+        {
+            _tileRenderer.color = _lightTileColour;
+        } else
+        {
+            _tileRenderer.color = _darkTileColour;
+        }
+    }
+
+    private Sprite DetermineSprite(string pieceCode)
+    {
+        switch(pieceCode)
+        {
+            case "K":
+                return _kingSpr;
+            case "Q":
+                return _queenSpr;
+            case "B":
+                return _bishopSpr;
+            case "R":
+                return _rookSpr;
+            case "Kn":
+                return _knightSpr;
+            default:
+            case "X":
+                return null;
+        }
+    }
+    
+}
---
+++ Technical Test SB/Assets/Scripts/GridCellResizer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(GridLayoutGroup))]
+public class GridCellResizer : MonoBehaviour
+{
+    private GridLayoutGroup _grid;
+    private RectTransform _rect;
+    private int _numCells, _numColumns;
+
+    private void Awake()
+    {
+        _grid = GetComponent<GridLayoutGroup>();
+        _rect = GetComponent<RectTransform>();
+    }
+
+    public void SetGrid(int numHorCells, int numVertCells)
+    {
+        _numColumns = numHorCells;
+        _numCells = Mathf.Max(numHorCells, numVertCells); // Account for the greatest size, since board doesn't have to be square
+        SizeCells();
+    }
+
+    private void SizeCells()
+    {
+        float cellSize = _rect.rect.width / _numCells;
+
+        _grid.cellSize = new Vector2(cellSize - _grid.spacing.x, cellSize - _grid.spacing.y);
+        _grid.constraintCount = _numColumns;
+    }
+}
